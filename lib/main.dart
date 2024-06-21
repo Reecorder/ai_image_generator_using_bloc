ac4fa6c4cd71bfd26dca6340e86adae461f6045d
Reetam Maity <reetam.maity@pixelconsultancy.in>
2024-06-21 18:05:09 +0530

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AI Image Generator ',
+      title: 'Artistry',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
